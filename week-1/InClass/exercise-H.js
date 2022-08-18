c21d7bc5cc63ff3d548eb1424d08940c2c80a4be
Xiomara Joya <xiomarajoya@gmail.com>
2022-08-18 20:18:43 +0200

    se realiza exercise H

---
+++ week-1/InClass/exercise-H.js
@@ -0,0 +1,13 @@
+function rectangularArea(base, height) {
+    return base * height; 
+}
+//Calculate the area of ​​a rectangle
+//return devuelve el valor de una operación.En cambio, console.log imprime lo que le siga dentro de los paréntesis
+console.log("Rectangular area: "+rectangularArea(10,30)+"ft²");
+
+
+function equilateralTriangleArea (base,height) {
+    return base * height / 2 
+}
+//Calculate the equilateral triangle area
+console.log("Equilateral triangle area: "+equilateralTriangleArea (5,9)+"cm²");
