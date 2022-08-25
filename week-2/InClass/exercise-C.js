f1d1c31c32531edc35129132b25cfc807411479e
Xiomara Joya <xiomarajoya@gmail.com>
2022-08-25 21:55:48 +0200

    exercise-C

--- week-2/InClass/exercise-C.js
+++ week-2/InClass/exercise-C.js
@@ -1,6 +1,6 @@
-function numberChecker(num) {
+function numberChecker(num) {// con el console.log usando num=50 se verifica en la terminal que el numero es mayor que 20; usando num=20 se verifica en la terminal que el numumero es igual a 20; usando num=10 se verifica en la terminal que el numumero es menor que 20;usando num= "x" se verifica el mensaje que no es un número
   if (num > 20) {
-    return `${num} is greater than 20`;
+    return `${num} is greater than 20`; 
   } else if (num === 20) {
     return `${num} is equal to 20`;
   } else if (num < 20) {
@@ -9,3 +9,4 @@ function numberChecker(num) {
     return `${num} isn't even a number :(`;
   }
 }
+console.log(numberChecker("a"));
