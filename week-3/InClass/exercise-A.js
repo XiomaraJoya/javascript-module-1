95c05e2d549e5e478558bfc5d328148926df1e12
Xiomara Joya <xiomarajoya@gmail.com>
2022-09-04 14:05:27 +0200

    exercises, A,C y D

--- week-3/InClass/exercise-A.js
+++ week-3/InClass/exercise-A.js
@@ -1,12 +1,14 @@
-const students = ["Xio", "Naty", "Linda", "Andrea", "Graciela", "Litzy"];
+let students = ["Xio", "Naty", "Linda", "Andrea", "Graciela", "Litzy",];
 
-console.log(students[0]);
-console.log(students[1]);
-console.log(students[2]);
-console.log(students[3]);
-console.log(students[4]);
-console.log(students[5]);
+students[0] = "Anna";
+students [5] = "Michel";
+
+console.log(students[0])
+console.log(students[1])
+console.log(students[2])
+console.log(students[3])
+console.log(students[4])
+console.log(students[5])
 
 console.log(students.length);
 
-students[0] = "Anna";
