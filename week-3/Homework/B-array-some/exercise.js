b1117edc108047dbfc68f3640ea4a151235c2c7d
Xiomara Joya <xiomarajoya@gmail.com>
2022-09-10 08:14:32 +0200

    B

--- week-3/Homework/B-array-some/exercise.js
+++ week-3/Homework/B-array-some/exercise.js
@@ -11,12 +11,12 @@ var pairsByIndex = [[0, 3], [1, 2], [2, 1], null, [3, 0]];
 // If there is a null value in the array exit the program with the error code
 // https://nodejs.org/api/process.html#process_process_exit_code
 // process.exit(1);
-const connull = pairsByIndex.some(element =>{
+const constNull = pairsByIndex.some(element =>{
   return element === null;
 });
 
-if(connull){
-  console.log("Hay un null en el array.");
+if(constNull){
+  console.log("There are null in the array.");
   process.exit(1);
 }
 
